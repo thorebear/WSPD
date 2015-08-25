813fee835e3686299d29fab8ae4b17f09b5ebc20
thorebear <cvcv90@gmail.com>
2015-08-25 12:40:38 +0200

    Added functionality to BoundingBox for 2D.

--- src/ProGAL/geom2d/BoundingBox.java
+++ src/ProGAL/geom2d/BoundingBox.java
@@ -1,6 +1,8 @@
 package ProGAL.geom2d;
 
+import ProGAL.dataStructures.Pair;
 import ProGAL.geom2d.viewer.J2DScene;
+import ProGAL.geom3d.*;
 
 import java.awt.*;
 import java.util.List;
@@ -9,10 +11,20 @@ public class BoundingBox {
 
     private Point leftTop, leftBottom, rightTop, rightBottom;
 
+    public BoundingBox(Point leftTop, Point leftBottom,
+                       Point rightTop, Point rightBottom)
+    {
+        this.leftTop = leftTop;
+        this.leftBottom = leftBottom;
+        this.rightTop = rightTop;
+        this.rightBottom = rightBottom;
+    }
+
     public BoundingBox(List<Point> points) {
         double leftBound, topBound, rightBound, bottomBound;
         if(points.isEmpty()){
-            throw new IllegalArgumentException("Bounding box for empty point set is not defined");
+            throw new IllegalArgumentException(
+                    "Bounding box for empty point set is not defined");
         }
 
         // Use the first point to get init values
@@ -41,5 +53,76 @@ public class BoundingBox {
         scene.addShape(new LineSegment(rightTop, rightBottom), col);
         scene.addShape(new LineSegment(leftTop, rightTop), col);
         scene.addShape(new LineSegment(leftBottom, rightBottom), col);
+    }
+
+    public int getDimensionWithMaxLength(){
+        if ((rightTop.x() - leftTop.x()) > (leftTop.y() - leftBottom.y())){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public Line getSplitLine(int dimension) {
+        if (dimension == 0){
+            return new Line((rightTop.x() - leftTop.x()) / 2, 0);
+        } else {
+            return new Line((leftTop.y() - leftBottom.y()) / 2, 0);
+        }
+    }
+
+    public Pair<BoundingBox, BoundingBox> split(Line splitLine) {
+        if (splitLine.n.y() == 0.0D){
+
+            if (splitLine.getPoint().x() < leftBottom.x() ||
+                    splitLine.getPoint().x() > rightBottom.x())
+            {
+                throw new IllegalArgumentException("The split line does not intersect with the bounding box");
+            }
+
+
+            BoundingBox left = new BoundingBox(
+                leftTop,
+                leftBottom,
+                new Point(splitLine.getPoint().x(), rightTop.y()),
+                new Point(splitLine.getPoint().x(), rightBottom.y())
+            );
+
+            BoundingBox right = new BoundingBox(
+                new Point(splitLine.getPoint().x(), leftTop.y()),
+                new Point(splitLine.getPoint().x(), leftBottom.y()),
+                rightTop,
+                rightBottom
+            );
+
+            return new Pair<>(left, right);
+
+        } else if (splitLine.n.x() == 0.0D){
+
+            if (splitLine.getPoint().y() < leftBottom.y() ||
+                    splitLine.getPoint().y() > leftTop.y())
+            {
+                throw new IllegalArgumentException("The split line does not intersect with the bounding box");
+            }
+
+            BoundingBox top = new BoundingBox(
+                leftTop,
+                new Point(leftBottom.x(), splitLine.getPoint().y()),
+                rightTop,
+                new Point(rightBottom.x(), splitLine.getPoint().y())
+            );
+
+            BoundingBox bottom = new BoundingBox(
+                new Point(leftTop.x(), splitLine.getPoint().y()),
+                leftBottom,
+                new Point(rightTop.x(), splitLine.getPoint().y()),
+                rightBottom
+            );
+
+            return new Pair<>(top, bottom);
+        } else {
+            throw new IllegalArgumentException(
+                "A bounding box can only be split by a vertical or horizontal line");
+        }
     }
 }
