c402555c3506f1c833e9655f0b5d23ceb04ffcbd
thorebear <cvcv90@gmail.com>
2015-09-22 09:19:11 +0200

    Added (a buggy??) version of bounding box for N-dimensions.

--- src/HelloProgal.java
+++ src/HelloProgal.java
@@ -1,36 +1,19 @@
 import ProGAL.dataStructures.Set;
-import ProGAL.geom2d.*;
-import ProGAL.geom2d.Point;
-import ProGAL.geom2d.viewer.J2DScene;
-import ProGAL.geom2d.wspd.SplitTree;
-import ProGAL.geom2d.wspd.WellSeparatedPairDecomposition;
+import ProGAL.geomNd.Point;
+import ProGAL.geomNd.BoundingBox;
 
-import java.awt.*;
 
 public class HelloProgal {
 
     public static void main(String[] args) throws InterruptedException {
         //Generate points
-        Set<Point> points = new PointSet();
-        for (int i = 0; i < 20; i++)
-            points.insert(new Point(Math.random(), Math.random()));
-
-        //Display them
-        J2DScene scene = J2DScene.createJ2DSceneInFrame();
-
-        for (Point p : points) {
-            //A filled circle with radius 0.01 and border 0
-            scene.addShape(new Circle(p, 0.01), Color.ORANGE, 0, true);
+        Set<Point> points = new Set<>();
+        for (int i = 0; i < 20; i++) {
+            points.insert(new Point(new double[]{Math.random(), Math.random()}));
         }
 
         BoundingBox box = new BoundingBox(points);
 
-        SplitTree tree = new SplitTree(points, box);
-        //tree.toScene(scene);
-
-        scene.centerCamera();
-        WellSeparatedPairDecomposition wspd = new WellSeparatedPairDecomposition(tree, points, 50000);
-        wspd.toScene(scene);
-        System.out.print(wspd.toString());
+        System.out.println(box.toString());
     }
 }
---
+++ src/ProGAL/geomNd/BoundingBox.java
@@ -0,0 +1,90 @@
+package ProGAL.geomNd;
+
+import ProGAL.dataStructures.Pair;
+import ProGAL.dataStructures.Set;
+
+import java.util.*;
+
+public class BoundingBox {
+
+    private List<Point> cornersPoints = new ArrayList<>();
+
+    public BoundingBox(Set<Point> points) {
+        if(points.isEmpty()){
+            throw new IllegalArgumentException(
+                    "Bounding box for empty point set is not defined");
+        }
+
+        Point firstPoint = points.get(0);
+        int dimension = firstPoint.getDimensions();
+
+        // Test that all points have the same dimensions.
+        for(int i = 1; i < points.getSize(); i++){
+            Point p = points.get(i);
+            if (p.getDimensions() != dimension){
+                throw new IllegalArgumentException("All points must be in the same dimension");
+            }
+        }
+
+        // Map each dimension to the lower and upper bound for this dimension.
+        Map<Integer, Pair<Double, Double>> bounds = new HashMap<>();
+
+        // Set init bound values, with the first point
+        for(int i = 0; i < dimension; i++)
+        {
+            bounds.put(i, new Pair<>(firstPoint.getCoord(i), firstPoint.getCoord(i)));
+        }
+
+        // Iterate through the rest of the points, and change the bounds, when a coordinate exceeds the existing bound
+        for(int pIndex = 1; pIndex < points.getSize(); pIndex++){
+            Point point = points.get(pIndex);
+            for(int dIndex = 0; dIndex < dimension; dIndex++){
+                double coordinate = point.getCoord(dIndex);
+                double lower = bounds.get(dIndex).fst;
+                double upper = bounds.get(dIndex).snd;
+                if (coordinate < lower) {
+                    bounds.put(dIndex, new Pair<>(coordinate, upper));
+                } else if (coordinate > upper) {
+                    bounds.put(dIndex, new Pair<>(lower, coordinate));
+                }
+            }
+        }
+
+        for (int dIndex = 0; dIndex < dimension; dIndex++){
+            double lowerBound = bounds.get(dIndex).fst;
+            double upperBound = bounds.get(dIndex).snd;
+
+            if (cornersPoints.size() == 0) {
+                Point fstPoint = new Point(new double[dimension]);
+                fstPoint.setCoord(dIndex, lowerBound);
+                cornersPoints.add(fstPoint);
+                Point sndPoint = new Point(new double[dimension]);
+                sndPoint.setCoord(dIndex, upperBound);
+                cornersPoints.add(sndPoint);
+            } else {
+                List<Point> pointsToAdd = new ArrayList<>();
+
+                for(Point cornerPoint : cornersPoints)
+                {
+                    // each points is splitted into two for each iteration
+                    Point newPoint = new Point(cornerPoint.getCoords());
+                    newPoint.setCoord(dIndex, lowerBound);
+                    cornerPoint.setCoord(dIndex, upperBound);
+                    pointsToAdd.add(newPoint);
+                }
+
+                cornersPoints.addAll(pointsToAdd);
+            }
+        }
+    }
+
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        for(Point point : cornersPoints)
+        {
+            builder.append(point.toString());
+        }
+        return builder.toString();
+    }
+
+}
